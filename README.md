f29d84952be864e82cdad0874e04d2e0ea11793f
Andrea Pinillos <andreapinillos@Andreas-MacBook-Pro.local>
2017-08-07 18:55:01 -0400

    javascript

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GifTastic
+
+A Web based Gif fetching application that uses the GIPHY API to fetch users' choice of GIFs. The user also has the option to add a new button of their favorite sport to be added to the top array of sports and those GIFs will be generated at the bottom. Each GIF has to be clicked to play/stop.
+
+Follow the link to view it live: andreapinillos.github.io/GifTastic/ 
---
+++ assets/javascript/main.js
@@ -0,0 +1,57 @@
+// Initial array of sports
+ var topics = ["basketball", "softball", "baseball", "football", "soccer", "rugby", "ultimate frisbee", "tennis", "gymnastics", "volleyball", "boxing"];
+
+$(document).ready(function() {
+    for (var i = 0; i < topics.length; i++) {
+        $("#sport-buttons").append("<button type='button' onclick='searchGif(\"" + topics[i] + "\")' class='btn btn-primary' value=' " + topics[i] + "'> " + topics[i] + " </button>");
+    }
+});
+
+function sportButtonClicked() {
+    var userInput = $('#sport-input').val();
+    searchGif(userInput);
+}
+
+function submitButtonClicked() {
+    var userInput = $('#sport-input').val();
+
+    if (userInput) {
+        $('#sport-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
+}
+
+function searchGif(gifName) {
+    $.ajax({
+            url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=b4a9605f0e4c42aeb2a6c6068f8fbde4&limit=10',
+            type: 'GET',
+        })
+        .done(function(response) {
+            displayGif(response);
+        })
+}
+
+function displayGif(response) {
+    $('#sports').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> <b>Rating:  </b>" + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#sports').append(image);
+    }
+
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
+    });
+}
+
